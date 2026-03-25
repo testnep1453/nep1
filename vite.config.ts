1f1d1345bc1427e5e230b18c7b804578e30c903f
testnep1453 <cunkumuhammedsaid@gmail.com>
2026-03-26 00:49:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nep/', // BURA ÇOK ÖNEMLİ! GitHub Pages bu klasör adını arar.
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     hmr: {
-      overlay: false // HMR error overlay'i kapat
+      overlay: false
     }
   },
-  clearScreen: false // Terminal ekranını temizlemeyi kapat
+  clearScreen: false
 });
